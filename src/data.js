89bf4c31ce24392c5da242ce1773401d9dc1dc22
unknown <lateefjeremiah@gmail.com>
2023-01-03 06:42:07 +0100

    Page: the page contains the data for the question & answer

---
+++ src/data.js
@@ -0,0 +1,29 @@
+const questions = [
+  {
+    id: 1,
+    question: `How many team members can I invite?`,
+    answer: "You can invite up to ten members",
+  },
+  {
+    id: 2,
+    question: `How many team members can I invite?`,
+    answer: "You can invite up to ten members",
+  },
+  {
+    id: 3,
+    question: `How many team members can I invite?`,
+    answer: "You can invite up to ten members",
+  },
+  {
+    id: 4,
+    question: `How many team members can I invite?`,
+    answer: "You can invite up to ten members",
+  },
+  {
+    id: 5,
+    question: `How many team members can I invite?`,
+    answer: "You can invite up to ten members",
+  },
+];
+
+export default questions;
